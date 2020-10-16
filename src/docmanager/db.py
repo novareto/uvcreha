fc9475d4bcf66b9784e2ccd78d9a3d6ba2e212c9
Souheil Chelfouh <trollfot@gmail.com>
2020-10-16 18:27:26 +0200

    Added users add/fetch

--- src/docmanager/db.py
+++ src/docmanager/db.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
 from cached_property import cached_property
 from arango import ArangoClient
+from functools import cached_property
 
 
 DB_CONFIG = namedtuple('DB', ['user', 'password', 'database'])
@@ -21,13 +22,16 @@ class Database:
         if not sys_db.has_database(self.config.database):
             sys_db.create_database(self.config.database)
 
-    @property
+    @cached_property
     def connector(self):
         return self.client.db(
             self.config.database,
             username=self.config.user,
             password=self.config.password
         )
+
+    def query(self, aql: str, *args, **kwargs):
+        return self.connector.AQLQuery(aql, *args, **kwargs)
 
     @classmethod
     def __get_validators__(cls):
