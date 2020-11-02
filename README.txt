d3fbd79e77c5ae11ad55b56708be66200d7e93b7
Souheil Chelfouh <trollfot@gmail.com>
2020-11-02 11:59:45 +0100

    Added delete folder. Renaming to match convention

--- README.txt
+++ README.txt
@@ -4,4 +4,6 @@ document manager
 curl -X POST localhost:8080/user.add --data username=test --data password=test
 curl -X POST localhost:8080/users/test/folder.add --data az=myfolder
 curl localhost:8080/users/test/folders
+curl localhost:8080/users/test/folders/myfolder
+curl -X DELETE localhost:8080/users/test/folders/myfolder
 curl localhost:8080/users/test/details
--- src/docmanager/browser/api/views.py
+++ src/docmanager/browser/api/views.py
@@ -1,15 +1,16 @@
 import orjson
 import horseman.response
 from roughrider.validation.types import Factory
-from docmanager.app import application
-from docmanager.layout import template, TEMPLATES
-from docmanager.request import Request
+
 from docmanager import db
+from docmanager.app import application
+from docmanager.request import Request
 from docmanager.browser import Namespace as NS
+from docmanager.browser.layout import template, TEMPLATES
 
 
 @application.route('/user.add', methods=['POST', 'PUT'], ns=NS.API)
-def add_user(request: Request, user: db.User):
+def user_add(request: Request, user: db.User):
     request.database_session.add(db.SQLUser(**user.dict()))
     request.database_session.commit()
     return horseman.response.json_reply(201, body={'id': user.username})
@@ -21,6 +22,14 @@ def user_view(user: Factory(db.SQLUser)):
     return horseman.response.reply(
         200, body=model.json(),
         headers={'Content-Type': 'application/json'})
+
+
+@application.route('/users/{userid}', methods=['DELETE'])
+def user_delete(request: Request, userid: str):
+    session = request.database_session
+    session.query(db.SQLUser).filter(SQLUser.username==userid).delete()
+    session.commit()
+    return horseman.response.reply(202)
 
 
 @application.route('/users/{userid}/details', methods=['GET'])
@@ -42,7 +51,27 @@ def user_folders(request: Request, userid: str):
 
 
 @application.route('/users/{username}/folder.add', methods=['POST', 'PUT'])
-def add_folder(request: Request, folder: db.Folder):
+def folder_add(request: Request, folder: db.Folder):
     request.database_session.add(db.SQLFolder(**folder.dict()))
     request.database_session.commit()
     return horseman.response.json_reply(201, body={'id': folder.az})
+
+
+@application.route('/users/{userid}/folders/{folderid}', methods=['GET'])
+def folder_view(request: Request, userid: str, folderid: str):
+    session = request.database_session
+    folder = session.query(db.SQLFolder).filter(
+        db.SQLFolder.username==userid, db.SQLFolder.az==folderid).first()
+    model = db.Folder.from_orm(folder)
+    return horseman.response.reply(
+        200, body=model.json(),
+        headers={'Content-Type': 'application/json'})
+
+
+@application.route('/users/{userid}/folders/{folderid}', methods=['DELETE'])
+def folder_delete(request: Request, userid: str, folderid: str):
+    session = request.database_session
+    session.query(db.SQLFolder).filter(
+        db.SQLFolder.username==userid, db.SQLFolder.az==folderid).delete()
+    session.commit()
+    return horseman.response.reply(202)
