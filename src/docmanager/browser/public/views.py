b374d4ad48f8aa9dc016643cbc9cd71a7bb5c37e
Souheil Chelfouh <trollfot@gmail.com>
2020-11-19 10:29:58 +0100

    Fixed test_index

--- src/docmanager/browser/public/views.py
+++ src/docmanager/browser/public/views.py
@@ -2,18 +2,12 @@ from horseman.response import Response
 from docmanager.app import application
 from docmanager.request import Request
 from docmanager.browser.layout import template, TEMPLATES
-
-
-@application.routes.register("/user")
-@template(TEMPLATES["index.pt"], layout_name="default", raw=False,)
-def user(request: Request):
-    request.flash().add(body="BLA BLUB")
-    request.app.plugins['amqp'].send('Some message', key='object.add')
-    return dict(request=request, user=None)
+from docmanager.db import User
 
 
 @application.routes.register("/")
 @template(TEMPLATES["index.pt"], layout_name="default", raw=False,)
-def index(request: Request):
-    request.app.plugins['amqp'].send('Some message', key='object.add')
-    return Response.create(200, body=b'Message sent')
+def user(request: Request):
+    request.flash().add(body="BLA BLUB")
+    user = User(request.db_session)
+    return dict(request=request, user=user)
