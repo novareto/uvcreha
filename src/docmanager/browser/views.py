729e280b3a41777a762583e3baa0cab4e3b69220
Souheil Chelfouh <trollfot@gmail.com>
2020-11-20 16:42:01 +0100

    Reshuffling the code to cleanup

--- src/docmanager/browser/views.py
+++ src/docmanager/browser/views.py
@@ -1,17 +1,19 @@
 import horseman.response
-from docmanager.app import api
-from docmanager.request import Request
+
+from docmanager.app import browser
 from docmanager.browser.layout import template, TEMPLATES
 from docmanager.browser.openapi import generate_doc
+from docmanager.db import User
+from docmanager.request import Request
 
 
-@api.routes.register('/doc')
+@browser.routes.register('/doc')
 @template(TEMPLATES['swagger.pt'], raw=False)
 def doc_swagger(request: Request):
     return {'url': '/openapi.json'}
 
 
-@api.routes.register('/openapi.json')
+@browser.routes.register('/openapi.json')
 def openapi(request: Request):
     open_api = generate_doc(request.app.routes)
     return horseman.response.reply(
@@ -19,3 +21,10 @@ def openapi(request: Request):
         body=open_api.json(by_alias=True, exclude_none=True, indent=2),
         headers={'Content-Type': 'application/json'}
     )
+
+
+@browser.route("/")
+@template(TEMPLATES["index.pt"], layout_name="default", raw=False)
+def index(request: Request):
+    user = User(request.db_session)
+    return dict(request=request, user=user)
