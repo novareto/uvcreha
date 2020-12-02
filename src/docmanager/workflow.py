69a35395c494f88e548a6ac3ba816baeed945294
Souheil Chelfouh <trollfot@gmail.com>
2020-12-02 19:04:31 +0100

    Wrote workflow for doc

--- src/docmanager/workflow.py
+++ src/docmanager/workflow.py
@@ -1,28 +1,45 @@
-from roughrider.workflow.meta import Workflow, State,
-from docmanager.models import Document
+from roughrider import workflow
 
 
-submitted = State(
-    title="Submitted",
-    identifier="myproject.submitted",
-    destinations={
-        'Publish': 'myproject.published'
-    }
-)
+def notify_trigger(item, request, **ns):
+    print(f'This is a notification: {item}, {request}.')
 
 
-published =  State(
-    title="Published",
-    identifier="myproject.published",
-    destinations={
-        'Re-submit': 'myproject.submitted'
-    }
-)
+class DocumentWorkflow(workflow.Workflow):
+
+    class wrapper(workflow.WorkflowItem):
+
+        def get_state(self):
+            return self.workflow.get(self.item.state)
+
+        def set_state(self, state):
+            if (error := self.check_reachable(state)):
+                raise error
+            self.item.state = state.name
+
+    class states(workflow.WorkflowState):
+        inquiry = 'Inquiry'
+        sent = 'Sent'
+        approved = 'Approved'
+
+    transitions = workflow.Transitions((
+        workflow.Transition(
+            origin=states.inquiry,
+            target=states.sent,
+            action=workflow.Action(
+                'Send',
+                triggers=[notify_trigger]
+            )
+        ),
+        workflow.Transition(
+            origin=states.sent,
+            target=states.approved,
+            action=workflow.Action(
+                'Approve',
+                triggers=[notify_trigger]
+            )
+        )
+    ))
 
 
-class DocumentWorkflow(Workflow):
-    pass
-
-
-DocumentWorkflow.register(Document, submitted, default=True)
-DocumentWorkflow.register(Document, published)
+document_workflow = DocumentWorkflow('inquiry')
