b9d395f50ca1be11133b2c962214c50849aa2f67
Christian Klinger <ck@novareto.de>
2020-12-03 16:56:53 +0100

    Start Tabed based Preferences

---
+++ src/docmanager/browser/preferences.py
@@ -0,0 +1,69 @@
+from docmanager.browser.layout import template, TEMPLATES
+from docmanager.browser.form import FormView, Form
+from docmanager.app import browser
+from reiter.form import trigger
+from docmanager.request import Request
+from horseman.http import Multidict
+import pydantic
+from docmanager.db import User
+import horseman.response
+
+
+class Kontaktdaten(pydantic.BaseModel):
+    name: str
+    surname: str
+
+
+class Account(pydantic.BaseModel):
+    name: str
+    iban: str
+
+
+@browser.route("/my_preferences")
+class MyPreferences(FormView):
+
+    title = "E-Mail Adresse ändern"
+    description = "Edit your preferences."
+    action = "my_preferences"
+
+    def setupForm(self, model, data={}, formdata=Multidict()):
+        form = Form.from_model(model)
+        form.process(data=data, formdata=formdata)
+        return form
+
+    @trigger("update", "Update", css="btn btn-primary", order=1)
+    def update(self, request, data):
+        data = data.form 
+        import pdb; pdb.set_trace()
+        form = self.setupForm(Kontaktdaten, formdata=data)
+        if not form.validate():
+            return form
+
+        user = User(request.db_session)
+        cd = request.user.preferences.dict()
+        cd.update(data.dict())
+        user.update(request.user.key, preferences=cd)
+        return horseman.response.reply(200)
+
+    @trigger("abbrechen", "Abbrechen", css="btn btn-secondary", order=20)
+    def abbrechen(self, request):
+        pass
+
+
+    @template(TEMPLATES["my_preferences.pt"], layout_name="default", raw=False)
+    def GET(self, request: Request):
+        contact = self.setupForm(Kontaktdaten)
+        account = self.setupForm(Account)
+
+        tabs = {
+            "Kontaktdaten": contact,
+            "Passwort": account,
+            "Datenschutz": contact,
+            "Benachrichtigungen": account,
+        }
+        return dict(request=request, tabs=tabs, view=self)
+
+    @template(TEMPLATES["my_preferences.pt"], layout_name="default", raw=False)
+    def POST(self, request: Request, **data):
+        request.extract()
+        return self.process_action(request)
