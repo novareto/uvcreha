4c15d7f50015bfa469834f23dce9a50934a6fc44
Souheil Chelfouh <trollfot@gmail.com>
2020-12-09 19:21:32 +0100

    Added webpush to the model via subscription

---
+++ src/docmanager/webpush.py
@@ -0,0 +1,24 @@
+import json
+from datetime import datetime, timedelta
+from typing import NamedTuple, Optional
+from pywebpush import webpush, WebPushException
+
+
+class Webpush(NamedTuple):
+    claims: dict
+    private_key: str
+    public_key: str
+
+    def send(self, token: str, message: str) -> Optional[str]:
+        info = json.loads(token)
+        ts = datetime.timestamp(datetime.now() + timedelta(hours=1))
+        claims = {**self.claims, 'exp': ts}
+        try:
+            webpush(
+                subscription_info=info,
+                data=message,
+                vapid_private_key=self.private_key,
+                vapid_claims=claims
+            )
+        except pywebpush.WebPushException as e:
+            return str(e)
