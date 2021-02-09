ab12fd880ddfbdc9878d71b97e15ddcbdc06dabb
Souheil Chelfouh <trollfot@gmail.com>
2021-02-09 11:29:25 +0100

    Added _db slot

--- src/docmanager/request.py
+++ src/docmanager/request.py
@@ -8,21 +8,23 @@ from reiter.application.request import Request as BaseRequest
 class Request(BaseRequest):
 
     __slots__ = (
+        '_db',
         'query',
     )
 
     def __init__(self, app, environ, route):
         super().__init__(app, environ, route)
+        self._db = None
         self.query = Query.from_environ(environ)
-            
+
     @property
     def session(self):
         return self.environ.get(self.app.config.env.session)
-            
+
     @property
     def user(self):
         return self.environ.get(self.app.config.env.user)
-    
+
     @property
     def database(self):
         if self._db is None:
