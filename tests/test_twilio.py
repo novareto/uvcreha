2b2d33ff4e543d3ca02ae02ac5b5fcbaa83ce0fa
Souheil CHELFOUH <trollfot@gmail.com>
2021-06-04 15:26:09 +0200

    cleaning up unneeded tests and tests utilities

--- tests/test_twilio.py
+++
@@ -1,7 +0,0 @@
-def test_twilio(webapp, session):
-    from twilio.rest import Client
-
-    app = webapp.__wrapped__
-    twilio = app.utilities.get('twilio')
-    assert twilio is not None
-    assert isinstance(twilio, Client) is True
