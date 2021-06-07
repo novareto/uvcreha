427c3ea920626f96ab64a9c1690aa20aaaced120
Souheil CHELFOUH <trollfot@gmail.com>
2021-06-07 12:11:59 +0200

    Pluggable edit form for document

--- src/uvcreha/browser/document.py
+++ src/uvcreha/browser/document.py
@@ -2,6 +2,7 @@ import horseman.response
 from horseman.http import Multidict
 from reiter.form import trigger
 from reiter.view.meta import View
+from reiter.application.registries import NamedComponents
 from uvcreha.app import browser
 from uvcreha.browser.layout import TEMPLATES
 from uvcreha.browser.form import Form, FormView
@@ -28,20 +29,31 @@ class DocumentIndex(View):
         return dict(request=self.request, document=self.context)
 
 
+DocumentEdit = NamedComponents()
+
+
 @browser.route(
-    "/users/{uid}/files/{az}/docs/{docid}/edit", name="doc.edit")
-class DocumentEditForm(FormView):
+    "/users/{uid}/files/{az}/docs/{docid}/edit",
+    methods=['GET', 'POST'],
+    name="doc.edit")
+def document_edit_dispatch(request, **params):
+    content_type = contenttypes.registry['document']
+    context = content_type.bind(request.database).find_one(**params)
+    form = DocumentEdit.get(context["content_type"], "default")
+    form.content_type = content_type
+    form.context = content_type.bind(request.database).find_one(**params)
+    return form(request, **params)
+
+
+@DocumentEdit.component('default')
+class DefaultDocumentEditForm(FormView):
     title = "Form"
     description = "Bitte füllen Sie alle Details"
-
-    def update(self):
-        ct = contenttypes.registry['document']
-        self.context = ct.bind(self.request.database).find_one(
-            **self.params)
+    content_type = None
+    context = None
 
     def setupForm(self, data={}, formdata=Multidict()):
-        schema = jsonschema.store.get(
-            self.context.data['content_type'])
+        schema = jsonschema.store.get(self.context['content_type'])
         form = Form.from_schema(schema)
         form.process(data=data, formdata=formdata)
         return form
@@ -51,7 +63,11 @@ class DocumentEditForm(FormView):
         form = self.setupForm(formdata=data.form)
         if not form.validate():
             return {'form': form}
-        raise NotImplementedError(form)
+        self.content_type.bind(self.request.database).update(
+            _key=self.context.id,
+            state=document_workflow.states.sent.name,
+            **data.form.dict()
+        )
         return horseman.response.redirect(
             self.request.environ['SCRIPT_NAME'] + '/'
         )
