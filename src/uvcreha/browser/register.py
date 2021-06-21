cec939a53bbac9ba13982b4179f20c0622efc27a
Christian Klinger <ck@novareto.de>
2021-06-21 11:11:17 +0200

    start with register

---
+++ src/uvcreha/browser/register.py
@@ -0,0 +1,100 @@
+from uvcreha.browser.crud import AddForm
+from uvcreha import contenttypes
+from uvcreha.browser.form import Form, FormView
+from uvcreha.workflow import user_workflow
+from uvcreha.app import browser
+from uvcreha.browser.layout import TEMPLATES
+from uuid import uuid4
+from urllib.parse import urlencode
+from horseman.http import Multidict
+from jsonschema_wtforms import schema_fields
+from uvcreha import contenttypes, jsonschema
+from wtforms import StringField
+from reiter.form import trigger
+
+
+TEXT = """Vielen Dank für Ihre Registrierung 
+Bitte klicken Sie auf folgenden Link %s
+"""
+
+
+
+@browser.register("/self_register", name="self_register")
+class AddUserForm(AddForm):
+    title = "Benutzer anlegen"
+    description = "Benutzer registrieren"
+
+    def update(self):
+        self.content_type = contenttypes.registry['user']
+
+    def create(self, data):
+        binding = self.content_type.bind(self.request.database)
+        data = data.form.dict()
+        uid = str(uuid4())
+        obj, response = binding.create(**{
+            **self.params,
+            **data,
+            '_key': uid,
+            'uid': uid,
+            'state': user_workflow.states.pending.name
+        })
+
+        token = obj.generate_token().now()
+        url = "https://reha.siguv.de%s?%s" % (self.request.route_path(name='verify_register'), urlencode(dict(uid=uid, token=token)))
+        print(url)
+        data = {}
+        self.request.app.notify(
+            "user_created",
+            request=self.request, uid=obj['uid'], user=obj)
+        return obj
+
+    def get_form(self):
+        return Form.from_schema(
+            self.content_type.schema,
+            include=("loginname", "password", "email")
+        )
+
+        
+@browser.register("/verify_register", name="verify_register")
+class VerifyRegistraion(FormView):
+    title = "Registrierung abschließen"
+    description = ""
+
+    def update(self):
+        self.content_type = contenttypes.registry['user']
+
+    def get_fields(self):
+        schema = jsonschema.store.get('User')
+        return schema_fields(schema, include=("password", "uid"))
+
+    def setupForm(self, data={}, formdata=Multidict()):
+        data = self.request.query.dict()
+        fields = self.get_fields()
+        fields['token'] = StringField('token')
+        form = Form(fields)
+        form.process(data=data, formdata=formdata)
+        return form
+
+    @trigger("speichern", "Registrierung abschließen", order=1, css="btn btn-primary")
+    def login(self, request, data):
+        form = self.setupForm(formdata=data.form)
+        flash_messages = request.utilities.get("flash")
+        if not form.validate():
+            return {"form": form}
+        binding = self.content_type.bind(self.request.database)
+        data = data.form.dict()
+        user = binding.find_one(uid=data['uid'])
+        if not user:
+            flash_messages.add(body="Kein Benutzer gefunden.")
+            return {"form": form}
+        token = user.generate_token()
+        if not token.verify(data['token']):
+            flash_messages.add(body="Fehler im Token.")
+            return {"form": form}
+        import pdb; pdb.set_trace()
+        if not data['password'] == user['password']:
+            flash_messages.add(body="Fehler Passwort.")
+            return {"form": form}
+        user.update(uid=data['uid'], state=user_workflow.states.active.name)
+        return self.redirect(request.environ["SCRIPT_NAME"] + "/")
+
